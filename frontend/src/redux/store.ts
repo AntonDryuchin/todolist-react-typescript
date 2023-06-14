b8f5be17416da53b7960eb0a6a0866badb60850e
Anton Dryuchin <antondryuchin@gmail.com>
2023-06-14 16:16:54 +0300

    redux added

---
+++ frontend/src/redux/store.ts
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import tasksSlice from "./tasks.slice";
+
+const store = configureStore({
+  reducer: { tasksSlice },
+});
+
+export default store;
