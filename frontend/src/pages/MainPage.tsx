721bd8eab2138623a53646a3cdb6ebcc28a8fa5b
Anton Dryuchin <antondryuchin@gmail.com>
2023-06-14 16:17:53 +0300

    main page

---
+++ frontend/src/pages/MainPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import List from "../components/molecules/List/List";
+import AddTaskForm from "../components/atoms/AddTaskForm/AddTaskForm";
+
+export default function MainPage() {
+  return (
+    <>
+      <h3>todo list</h3>
+      <AddTaskForm type="add" task={{ id: 0, text: "", isDone: false }} />
+      <List />
+    </>
+  );
+}
